5f722902f2f384e635706730d6bedabea6ca8366
NKYB <3k2bar@gmail.com>
2014-11-11 22:24:06 -0500

    Updated kernel test functions

--- src/test/java/com/amodulus/jocl/kernels/kernel_func.java
+++ src/test/java/com/amodulus/jocl/kernels/kernel_func.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.amodulus.jocl.kernels;
 
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 import com.amodulus.jocl.utils.JOCLHelper;
 import java.io.IOException;
 import junit.framework.TestCase;
@@ -16,20 +7,12 @@ import org.jocl.Pointer;
 import org.jocl.Sizeof;
 import static org.jocl.CL.*;
 
-
-/**
- *
- * @author Administrator
- */
 public class kernel_func extends TestCase {
     
     public kernel_func(String testName) {
         super(testName);
     }
 
-    /**
-     * Test of initDevice method, of class JOCLHelper.
-     */
     public void testKernel() throws IOException {
         System.out.println("testKernel");
         
@@ -45,17 +28,14 @@ public class kernel_func extends TestCase {
         
         JOCLHelper joclHelper = new JOCLHelper();
         joclHelper.initDevice(n,n,platformIndex,deviceIndex);
-//        joclHelper.initDevice(1,1,platformIndex,deviceIndex);
         joclHelper.addVar(CL_MEM_READ_WRITE, Sizeof.cl_float * n, null);
         joclHelper.createKernel(kernelName, programSource);
+        
         long startTime = System.currentTimeMillis();
         joclHelper.runKernel();
-        
         joclHelper.loadOutput(dst, 1, n * Sizeof.cl_float);
         long endTime = System.currentTimeMillis();
-        
-        System.out.print("GUID: " + dstArray[0]);
-        System.out.println(" time to run in Millis: " + (endTime - startTime) );
+        System.out.println("Time to run kernel in (millis): " + (endTime - startTime) );
 
         assertEquals(dstArray[0],100);
     }
